89dda664108d100697cad39b6faa5255f27239bc
Wojtek Przerwa <czescwojtek@gmail.com>
2019-10-13 18:51:31 +0200

    gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,6 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/img`,
-      },
-    },
-    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'pages',
